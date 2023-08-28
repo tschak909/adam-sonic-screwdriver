7a02ac75353eda8c4e908c4ae0f29fbce32932ed
Thomas <thom.cherryhomes@gmail.com>
2023-08-28 08:38:56 -0500

    start filling in.

---
+++ src/main.c
@@ -0,0 +1,11 @@
+/**
+ * @brief   Adam's Disk Sonic Screwdriver - a File and Disk Manager
+ * @author  Thomas Cherryhomes
+ * @email   thom dot cherryhomes at gmail dot com
+ * @license gpl v. 3, see LICENSE for details.
+ * @verbose Main entrypoint
+ */
+
+void main(void)
+{
+}
