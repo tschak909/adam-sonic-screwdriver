368d4a391c58ebe447667ab359c32c043166877a
Thomas <thom.cherryhomes@gmail.com>
2023-10-30 17:54:04 -0500

    add test script.

---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+~/Vintage/Coleco\ Adam/adamem -vi 3 -tapea adam-disk-sonic-screwdriver.ddp -tapeb ~/Workspace/tnfs-backup/tnfs/Coleco_Adam/Productivity/ADAMCalc\ \(1984\)\ \(Coleco\).ddp
